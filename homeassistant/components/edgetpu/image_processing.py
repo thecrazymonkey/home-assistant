80c9b15702ae3829e295dd565b755bf74085c69c
thecrazymonkey <ivan.kunz@gmail.com>
2019-04-22 21:27:28 -0400

    face detection using Coral Edge TPU

--- homeassistant/components/edgetpu/image_processing.py
+++ homeassistant/components/edgetpu/image_processing.py
@@ -1,7 +1,5 @@
 """Support for performing EdgeTPU classification on images."""
 import logging
-import os
-import sys
 
 import voluptuous as vol
 
@@ -21,64 +19,43 @@ ATTR_TOTAL_MATCHES = 'total_matches'
 CONF_CATEGORIES = 'categories'
 CONF_CATEGORY = 'category'
 CONF_FILE_OUT = 'file_out'
-CONF_GRAPH = 'graph'
+CONF_PATH = 'path'
 CONF_LABELS = 'labels'
 CONF_MODEL = 'model'
 CONF_MODEL_DIR = 'model_dir'
-
+CONF_TPU_DEVICE = 'device'
 CONF_TPU_TOP_K = 'top_k'
 CONF_TPU_THRESHOLD = 'threshold'
-CONF_TPU_DEVICE = 'device'
+CONF_TPU_KEEP_ASPECT_RATIO = 'keep_aspect_ratio'
+CONF_TPU_RESAMPLE = 'resample'
+
+DEFAULT_THRESHOLD = 0.05
+DEFAULT_TOP_K = 10
+DEFAULT_KEEP_ASPECT_RATIO = True
+DEFAULT_RESAMPLE = 0
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_FILE_OUT, default=[]):
         vol.All(cv.ensure_list, [cv.template]),
     vol.Required(CONF_MODEL): vol.Schema({
-        vol.Required(CONF_GRAPH): cv.isfile,
+        vol.Required(CONF_PATH): cv.isfile,
         vol.Optional(CONF_LABELS): cv.isfile,
         vol.Optional(CONF_MODEL_DIR): cv.isdir,
         vol.Optional(CONF_TPU_DEVICE): cv.string,
-        vol.Optional(CONF_TPU_THRESHOLD): cv.small_float,
-        vol.Optional(CONF_TPU_TOP_K): cv.positive_int,
+        vol.Optional(CONF_TPU_THRESHOLD, default=DEFAULT_THRESHOLD): cv.small_float,
+        vol.Optional(CONF_TPU_KEEP_ASPECT_RATIO, default=DEFAULT_KEEP_ASPECT_RATIO): cv.boolean,
+        vol.Optional(CONF_TPU_RESAMPLE, default=DEFAULT_RESAMPLE): cv.positive_int,
+        vol.Optional(CONF_TPU_TOP_K, default=DEFAULT_TOP_K): cv.positive_int,
     })
 })
 
-
-def draw_box(draw, box, img_width,
-             img_height, text='', color=(255, 255, 0)):
-    """Draw bounding box on image."""
-    ymin, xmin, ymax, xmax = box
-    (left, right, top, bottom) = (xmin * img_width, xmax * img_width,
-                                  ymin * img_height, ymax * img_height)
-    draw.line([(left, top), (left, bottom), (right, bottom),
-               (right, top), (left, top)], width=5, fill=color)
-    if text:
-        draw.text((left, abs(top-15)), text, fill=color)
-
-
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the TensorFlow image processing platform."""
-    model_config = config.get(CONF_MODEL)
-    model_dir = model_config.get(CONF_MODEL_DIR) \
-        or hass.config.path('edgetpu')
-    labels = model_config.get(CONF_LABELS) \
-        or hass.config.path('edgetpu', 'object_detection',
-                            'data', 'coco_labels.txt')
-
-    # Make sure locations exist
-    if not os.path.isdir(model_dir) or not os.path.exists(labels):
-        _LOGGER.error("Unable to locate edgetpu models or label map")
-        return
-
-    # append custom model path to sys.path
-    sys.path.append(model_dir)
+    """Set up the EdgeTPU image processing platform."""
 
     try:
         # Verify that the TensorFlow Object Detection API is pre-installed
         # pylint: disable=unused-import,unused-variable
         from edgetpu.detection.engine import DetectionEngine
-        from PIL import Image
-        from PIL import ImageDraw
     except ImportError:
         # pylint: disable=line-too-long
         _LOGGER.error(
@@ -94,23 +71,33 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     add_entities(entities)
 
-
 class EdgeTPUImageProcessor(ImageProcessingEntity):
     """Representation of an EdgeTPU image processor."""
 
     def __init__(self, hass, camera_entity, name, config):
         """Initialize the EdgeTPU entity."""
+        from edgetpu.detection.engine import DetectionEngine # pylint: disable=import-error
         model_config = config.get(CONF_MODEL)
+        _LOGGER.info("config = %s", model_config)
         self.hass = hass
         self._camera_entity = camera_entity
+        _LOGGER.info("camera = %s", self._camera_entity)
         if name:
             self._name = name
         else:
             self._name = "EdgeTPU {0}".format(
                 split_entity_id(camera_entity)[1])
-        self._min_confidence = config.get(CONF_CONFIDENCE)
         self._file_out = config.get(CONF_FILE_OUT)
+        self._model = model_config.get(CONF_PATH)
+        self._threshold = model_config.get(CONF_TPU_THRESHOLD)
+        self._top_k = model_config.get(CONF_TPU_TOP_K)
+        self._keep_aspect_ratio = model_config.get(CONF_TPU_KEEP_ASPECT_RATIO)
+        self._resample = model_config.get(CONF_TPU_RESAMPLE)
+        self._engine = DetectionEngine(self._model, device_path=model_config.get(CONF_TPU_DEVICE))
+        labels = model_config.get(CONF_LABELS)
+        self._labels = self._read_label_file(labels) if labels else None
 
+        template.attach(hass, self._file_out)
 
         self._matches = {}
         self._total_matches = 0
@@ -136,122 +123,68 @@ class EdgeTPUImageProcessor(ImageProcessingEntity):
         """Return device specific state attributes."""
         return {
             ATTR_MATCHES: self._matches,
-            ATTR_SUMMARY: {category: len(values)
-                           for category, values in self._matches.items()},
+#            ATTR_SUMMARY: {item: len(values)
+#                           for item, values in self._matches.items()},
             ATTR_TOTAL_MATCHES: self._total_matches
         }
 
-    def _save_image(self, image, matches, paths):
-        from PIL import Image, ImageDraw
-        import io
-        img = Image.open(io.BytesIO(bytearray(image))).convert('RGB')
-        img_width, img_height = img.size
-        draw = ImageDraw.Draw(img)
-
-        # Draw custom global region/area
-        if self._area != [0, 0, 1, 1]:
-            draw_box(draw, self._area,
-                     img_width, img_height, "Detection Area", (0, 255, 255))
-
-        for category, values in matches.items():
-            # Draw custom category regions/areas
-            if (category in self._category_areas
-                    and self._category_areas[category] != [0, 0, 1, 1]):
-                label = "{} Detection Area".format(category.capitalize())
-                draw_box(
-                    draw, self._category_areas[category], img_width,
-                    img_height, label, (0, 255, 0))
-
-            # Draw detected objects
-            for instance in values:
-                label = "{0} {1:.1f}%".format(category, instance['score'])
-                draw_box(
-                    draw, instance['box'], img_width, img_height, label,
-                    (255, 255, 0))
-
-        for path in paths:
-            _LOGGER.info("Saving results image to %s", path)
-            img.save(path)
+    # Function to read labels from text files.
+    def _read_label_file(self, file_path):
+        with open(file_path, 'r', encoding="utf-8") as source_file:
+            lines = source_file.readlines()
+        ret = {}
+        for line in lines:
+            pair = line.strip().split(maxsplit=1)
+            ret[int(pair[0])] = pair[1].strip()
+        return ret
 
     def process_image(self, image):
         """Process the image."""
-        import numpy as np
-
-        try:
-            import cv2  # pylint: disable=import-error
-            img = cv2.imdecode(
-                np.asarray(bytearray(image)), cv2.IMREAD_UNCHANGED)
-            inp = img[:, :, [2, 1, 0]]  # BGR->RGB
-            inp_expanded = inp.reshape(1, inp.shape[0], inp.shape[1], 3)
-        except ImportError:
-            from PIL import Image
-            import io
-            img = Image.open(io.BytesIO(bytearray(image))).convert('RGB')
-            img.thumbnail((460, 460), Image.ANTIALIAS)
-            img_width, img_height = img.size
-            inp = np.array(img.getdata()).reshape(
-                (img_height, img_width, 3)).astype(np.uint8)
-            inp_expanded = np.expand_dims(inp, axis=0)
-
-        image_tensor = self._graph.get_tensor_by_name('image_tensor:0')
-        boxes = self._graph.get_tensor_by_name('detection_boxes:0')
-        scores = self._graph.get_tensor_by_name('detection_scores:0')
-        classes = self._graph.get_tensor_by_name('detection_classes:0')
-        boxes, scores, classes = self._session.run(
-            [boxes, scores, classes],
-            feed_dict={image_tensor: inp_expanded})
-        boxes, scores, classes = map(np.squeeze, [boxes, scores, classes])
-        classes = classes.astype(int)
+        from PIL import Image
+        from PIL import ImageDraw
+        _LOGGER.debug("Model=%s", self._model)
 
         matches = {}
         total_matches = 0
-        for box, score, obj_class in zip(boxes, scores, classes):
-            score = score * 100
-            boxes = box.tolist()
 
-            # Exclude matches below min confidence value
-            if score < self._min_confidence:
-                continue
+        # Open image.
+#        _LOGGER.info("image = %s", image)
+        import io
+        img = Image.open(io.BytesIO(bytearray(image)))
+#        img.save("/tmp/test.jpg")
+        draw = ImageDraw.Draw(img)
 
-            # Exclude matches outside global area definition
-            if (boxes[0] < self._area[0] or boxes[1] < self._area[1]
-                    or boxes[2] > self._area[2] or boxes[3] > self._area[3]):
-                continue
-
-            category = self._category_index[obj_class]['name']
-
-            # Exclude unlisted categories
-            if (self._include_categories
-                    and category not in self._include_categories):
-                continue
-
-            # Exclude matches outside category specific area definition
-            if (self._category_areas
-                    and (boxes[0] < self._category_areas[category][0]
-                         or boxes[1] < self._category_areas[category][1]
-                         or boxes[2] > self._category_areas[category][2]
-                         or boxes[3] > self._category_areas[category][3])):
-                continue
-
-            # If we got here, we should include it
-            if category not in matches.keys():
-                matches[category] = []
-            matches[category].append({
-                'score': float(score),
-                'box': boxes
-            })
-            total_matches += 1
-
-        # Save Images
-        if total_matches and self._file_out:
-            paths = []
-            for path_template in self._file_out:
-                if isinstance(path_template, template.Template):
-                    paths.append(path_template.render(
-                        camera_entity=self._camera_entity))
-                else:
-                    paths.append(path_template)
-            self._save_image(image, matches, paths)
+        # Run inference.
+        ans = self._engine.DetectWithImage(img, threshold=self._threshold,
+                                           keep_aspect_ratio=self._keep_aspect_ratio,
+                                           relative_coord=False, top_k=self._top_k)
+        # Display result.
+        if ans:
+            for obj in ans:
+                _LOGGER.info("obj = %s", obj)
+                if self._labels:
+                    _LOGGER.info(self._labels[obj.label_id])
+                _LOGGER.info("score = %f", obj.score)
+                box = obj.bounding_box.flatten().tolist()
+                _LOGGER.info("box = %s", box)
+                # Draw a rectangle.
+                draw.rectangle(box, outline='red')
+                if self._file_out:
+                    for path_template in self._file_out:
+                        if isinstance(path_template, template.Template):
+                            img.save(path_template.render(
+                                camera_entity=self._camera_entity))
+                        else:
+                            img.save(path_template)
+                if 'Face' not in matches.keys():
+                    matches['Face'] = []
+                matches['Face'].append({
+                    'score': float(obj.score),
+                    'box': box
+                })
+                total_matches += 1
+        else:
+            _LOGGER.info("No object detected!")
 
         self._matches = matches
         self._total_matches = total_matches
